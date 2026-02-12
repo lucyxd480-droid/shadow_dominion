ff8046d5d45f6306ba0d5d5d6c5fc63e2561f8d0
Lucky <lucyxd480@gmail.com>
2026-02-13 03:42:13 +0530

    Create pure.py

---
+++ roles/pure.py
@@ -0,0 +1,31 @@
+from roles.base import Role
+
+class Luminary(Role):
+    name = "Luminary"
+    faction = "pure"
+    priority = 0
+
+class Oracle(Role):
+    name = "Oracle"
+    faction = "pure"
+    priority = 1
+
+class Guardian(Role):
+    name = "Guardian"
+    faction = "pure"
+    priority = 5
+
+class Hunter(Role):
+    name = "Hunter"
+    faction = "pure"
+    priority = 6
+
+class Watcher(Role):
+    name = "Watcher"
+    faction = "pure"
+    priority = 2
+
+class Martyr(Role):
+    name = "Martyr"
+    faction = "pure"
+    priority = 7
