1464380d4eb6294a7bdf8e5171c67d8329cc4e3d
Lucky <lucyxd480@gmail.com>
2026-02-13 05:59:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
-API_ID = 123456
-API_HASH = "YOUR_API_HASH"
-BOT_TOKEN = "YOUR_BOT_TOKEN"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 JOIN_TIME = 40
 NIGHT_TIME = 45
